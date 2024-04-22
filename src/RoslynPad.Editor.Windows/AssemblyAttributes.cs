3c67fe568b1383bc241e9c7b0e9085987cc7bee4
codwrench <coddwrench@gmail.com>
2024-04-23 01:38:05 +0300

    remove windows

--- src/RoslynPad.Editor.Windows/AssemblyAttributes.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-using System.Windows;
-
-[assembly: ThemeInfo(
-    themeDictionaryLocation: ResourceDictionaryLocation.None,
-    genericDictionaryLocation: ResourceDictionaryLocation.SourceAssembly
-)]
-
-[assembly: InternalsVisibleTo("RoslynPad")]
